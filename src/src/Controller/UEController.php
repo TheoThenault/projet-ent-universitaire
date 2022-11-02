7b1b5869080dc9f5c73f7f2e85600ecd1b56dcdb
MZASU <ttrider86@gmail.com>
2022-11-02 16:28:06 +0100

    Création des fichiers de base du controller Avec le maker

---
+++ src/src/Controller/UEController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/ue', name: 'ue_')]
+class UEController extends AbstractController
+{
+    #[Route('', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('ue/index.html.twig', [
+            'controller_name' => 'UEController',
+        ]);
+    }
+}
