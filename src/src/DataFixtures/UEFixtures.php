4090d6e9fbd9de19d4a069094fd7a2a96def459e
tthenaul <theo.thenault@etu.univ-poitiers.fr>
2022-11-03 11:52:36 +0100

    Ajout des UEs dans la BDD et des fixtures des UEs

--- src/src/DataFixtures/UEFixtures.php
+++ src/src/DataFixtures/UEFixtures.php
@@ -5,12 +5,41 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+use App\Entity\UE;
+
 class UEFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $ue = new UE();
+        $ue->setNom("POO");
+        $ue->setVolumeHoraire(75);
+        $manager->persist($ue);
+
+        $ue = new UE();
+        $ue->setNom("Algorithmes");
+        $ue->setVolumeHoraire(75);
+        $manager->persist($ue);
+
+        $ue = new UE();
+        $ue->setNom("Anglais");
+        $ue->setVolumeHoraire(150);
+        $manager->persist($ue);
+
+        $ue = new UE();
+        $ue->setNom("Droits");
+        $ue->setVolumeHoraire(25);
+        $manager->persist($ue);
+
+        $ue = new UE();
+        $ue->setNom("Web");
+        $ue->setVolumeHoraire(75);
+        $manager->persist($ue);
+
+        $ue = new UE();
+        $ue->setNom("Web avancé");
+        $ue->setVolumeHoraire(75);
+        $manager->persist($ue);
 
         $manager->flush();
     }
