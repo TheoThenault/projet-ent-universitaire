da9326093a014a8bbb2d8a4f90369c2fe3dd2e94
tthenaul <theo.thenault@etu.univ-poitiers.fr>
2022-11-03 11:43:12 +0100

    Création entité UE

---
+++ src/src/Entity/UE.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UERepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UERepository::class)]
+class UE
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $volumeHoraire = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getVolumeHoraire(): ?int
+    {
+        return $this->volumeHoraire;
+    }
+
+    public function setVolumeHoraire(?int $volumeHoraire): self
+    {
+        $this->volumeHoraire = $volumeHoraire;
+
+        return $this;
+    }
+}
