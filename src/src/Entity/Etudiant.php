327f81ccd3d2bbb447bf58c08b34f2244b299497
MathieuDlp <contact.mathieudelepine@gmail.com>
2022-11-03 10:27:54 +0100

    Create Personne, Enseignant, Etudiant Entity to populate BDD.

---
+++ src/src/Entity/Etudiant.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtudiantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EtudiantRepository::class)]
+class Etudiant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne(mappedBy: 'etudiant', cascade: ['persist', 'remove'])]
+    private ?Personne $personne = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPersonne(): ?Personne
+    {
+        return $this->personne;
+    }
+
+    public function setPersonne(?Personne $personne): self
+    {
+        // unset the owning side of the relation if necessary
+        if ($personne === null && $this->personne !== null) {
+            $this->personne->setEtudiant(null);
+        }
+
+        // set the owning side of the relation if necessary
+        if ($personne !== null && $personne->getEtudiant() !== $this) {
+            $personne->setEtudiant($this);
+        }
+
+        $this->personne = $personne;
+
+        return $this;
+    }
+}
