26ee89a0204880d6ca603a3339bc4af845204be2
tthenaul <theo.thenault@etu.univ-poitiers.fr>
2022-11-03 15:53:41 +0100

    Modification du fichier UEFixtures et AppFixtures afin d'être plus simple à utiliser

--- src/src/DataFixtures/AppFixtures.php
+++ src/src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\DataFixtures\UEFixtures;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +12,14 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $ues = new UEFixtures();
+        $ues->charger($manager);
+
+        for($i = 0; $i < count($ues->list_ues); $i++)
+        {
+            dump($ues->list_ues[$i]);
+        }
 
         $manager->flush();
     }
--- src/src/DataFixtures/UEFixtures.php
+++ src/src/DataFixtures/UEFixtures.php
@@ -2,44 +2,51 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 use App\Entity\UE;
 
-class UEFixtures extends Fixture
+class UEFixtures
 {
-    public function load(ObjectManager $manager): void
+    public array $list_ues = array();
+
+    public function charger(ObjectManager $manager): void
     {
         $ue = new UE();
         $ue->setNom("POO");
         $ue->setVolumeHoraire(75);
         $manager->persist($ue);
+        $this->list_ues[] = $ue;
 
         $ue = new UE();
         $ue->setNom("Algorithmes");
         $ue->setVolumeHoraire(75);
         $manager->persist($ue);
+        $this->list_ues[] = $ue;
 
         $ue = new UE();
         $ue->setNom("Anglais");
         $ue->setVolumeHoraire(150);
         $manager->persist($ue);
+        $this->list_ues[] = $ue;
 
         $ue = new UE();
         $ue->setNom("Droits");
         $ue->setVolumeHoraire(25);
         $manager->persist($ue);
+        $this->list_ues[] = $ue;
 
         $ue = new UE();
         $ue->setNom("Web");
         $ue->setVolumeHoraire(75);
         $manager->persist($ue);
+        $this->list_ues[] = $ue;
 
         $ue = new UE();
         $ue->setNom("Web avancé");
         $ue->setVolumeHoraire(75);
         $manager->persist($ue);
+        $this->list_ues[] = $ue;
 
         $manager->flush();
     }
