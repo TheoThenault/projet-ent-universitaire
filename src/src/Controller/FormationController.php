d48f2968252238082f4d6ee95713c00b2af1f133
MathieuDlp <contact.mathieudelepine@gmail.com>
2023-01-06 16:26:17 +0100

    Added Formation addition .

--- src/src/Controller/FormationController.php
+++ src/src/Controller/FormationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Cursus;
 use App\Entity\Formation;
+use App\Form\formation\FormationAddType;
 use App\Form\formation\FormationFilterType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -48,6 +49,27 @@ class FormationController extends AbstractController
         return $this->render('formation/index.html.twig', [
             'formationFormulaire' => $form->createView(),
             'liste_formations' => $liste_formations
+        ]);
+    }
+
+    #[Route('/add', name: 'add')]
+    public function addAction(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $formation = new Formation();
+        $form = $this->createForm(FormationAddType::class, $formation);
+        $form->add('send', SubmitType::class, ['label' => 'Ajouter une nouvelle formation']);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->persist($formation);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('formation_index');
+        }
+
+        return $this->render('formation/add.html.twig', [
+            'addFormationForm' => $form->createView(),
         ]);
     }
 }
