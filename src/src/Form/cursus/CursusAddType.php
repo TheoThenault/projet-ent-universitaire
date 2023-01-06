405bbb46966d5d77647ab86c88f3cc5f713c0d00
MathieuDlp <contact.mathieudelepine@gmail.com>
2023-01-06 15:31:21 +0100

    Added Cursus addition .

---
+++ src/src/Form/cursus/CursusAddType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form\cursus;
+
+use App\Entity\Cursus;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CursusAddType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'help' => 'Droit, Informatique, Médecine ...',
+            ])
+            ->add('niveau', ChoiceType::class, [
+            'choices'  => [
+                'Licence' => 'Licence',
+                'Master' => 'Master'
+            ],
+        ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Cursus::class,
+        ]);
+    }
+}
