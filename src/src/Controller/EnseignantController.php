09efa196737131049ef8122791af31365cd55cba
TheoThenault <theo.thenault@gmail.com>
2023-01-31 14:32:35 +0100

    Effacer tout

--- src/src/Controller/EnseignantController.php
+++ src/src/Controller/EnseignantController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Enseignant;
 use App\Form\enseignant\EnseignantFilterType;
+use App\Form\enseignant\RechercheProfType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -18,9 +19,9 @@ class EnseignantController extends AbstractController
     #[Route('/list', name: 'list')]
     public function index(EntityManagerInterface $entityManager, Request $request): Response
     {
-        $enseignants =  $entityManager->getRepository(Enseignant::class)->findAll();
+        //$enseignants =  $entityManager->getRepository(Enseignant::class)->findAll();
 
-        $form = $this->createForm(EnseignantFilterType::class);
+        $form = $this->createForm(RechercheProfType::class);
         $form->add('send', SubmitType::class, ['label' => 'Filter']);
         $form->handleRequest($request);
 
@@ -28,41 +29,9 @@ class EnseignantController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $responses = $form->getData();
             dump($responses);
-            $choix_statut = $form->get("statut_enseignant")->getData()->getNom();
-            $enseignants = $entityManager->getRepository(Enseignant::class)->filterByStatut($choix_statut);
 
-            switch($responses["sort_asc_or_desc"]) {
-                case "email":
-                    $enseignants = $entityManager->getRepository(Enseignant::class)->sortByEmailAscOrDesc("ASC");
-                    break;
-                case "email_desc":
-                    $enseignants = $entityManager->getRepository(Enseignant::class)->sortByEmailAscOrDesc("DESC");
-                    break;
-                case "nom":
-                    $enseignants = $entityManager->getRepository(Enseignant::class)->sortByNameAscOrDesc("ASC");
-                    break;
-                case "nom_desc":
-                    $enseignants = $entityManager->getRepository(Enseignant::class)->sortByNameAscOrDesc("DESC");
-                    break;
-                case "prenom":
-                    $enseignants = $entityManager->getRepository(Enseignant::class)->sortByNameAscOrDesc("ASC");
-                    break;
-                case "prenom_desc":
-                    $enseignants = $entityManager->getRepository(Enseignant::class)->sortByNameAscOrDesc("DESC");
-                    break;
-                default:
-                    break;
-            }
-
-            return $this->render('enseignant/list.twig', [
-                'enseignants' => $enseignants,
-                'enseignantFilterForm' => $form->createView(),
-            ]);
         }
 
-        return $this->render('enseignant/list.twig', [
-            'enseignants' => $enseignants,
-            'enseignantFilterForm' => $form->createView(),
-        ]);
+        return $this->render('enseignant/list.twig');
     }
 }
