cd25c57a5dac6c02e0defc8285a099d53384fd3b
TheoThenault <theo.thenault@gmail.com>
2023-01-31 18:08:36 +0100

    4000 Cours

--- src/src/DataFixtures/CourFixtures.php
+++ src/src/DataFixtures/CourFixtures.php
@@ -19,18 +19,22 @@ class CourFixtures
         $nS = count($list_salles);
         $nU = count($list_ues);
 
-        $nombre_cours = 40;
+        $nombre_cours = 4000;
 
         for($i = 0; $i < $nombre_cours; $i++)
         {
+            $prof = rand(0, $nE-1);
+            $sall = rand(0, $nS-1);
+            $_ue = rand(0, $nU-1);
+
             $cour = new Cour();
-            $cour->setCreneau(($i % 40) + 1);
-            $cour->setEnseignant($list_enseignant[$i % $nE]);
-            $cour->setSalle($list_salles[$i%$nS]);
-            $cour->setUe($list_ues[1]);
+            $cour->setCreneau(($i % 600) + 1);
+            $cour->setEnseignant($list_enseignant[$prof]);
+            $cour->setSalle($list_salles[$sall]);
+            $cour->setUe($list_ues[$_ue]);
             for($j = 0; $j < count($list_groupes); $j++)
             {
-                if($list_groupes[$j]->getFormation() == $list_ues[1]->getFormation())
+                if($list_groupes[$j]->getFormation() == $list_ues[$_ue]->getFormation())
                 {
                     $cour->setGroupe($list_groupes[$j]);
                     break;
