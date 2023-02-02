f999ecd0d63451afbf31b674121866dee3295c68
TheoThenault <theo.thenault@gmail.com>
2023-02-02 10:38:21 +0100

    UeValides fix

--- src/src/DataFixtures/AppFixtures.php
+++ src/src/DataFixtures/AppFixtures.php
@@ -23,11 +23,17 @@ class AppFixtures extends Fixture
         $salleFixture = new SalleFixtures();
         $salleFixture->charger($manager);
 
+        var_dump('salle fini');
+
         $personnes_fixture = new PersonneFixtures();
         $personnes_fixture->charger($manager);
 
+        var_dump('personne fini');
+
         $statuts_enseignant_fixture = new StatutEnseignantFixtures();
         $statuts_enseignant_fixture->charger($manager);
+
+        var_dump('status_enseignant fini');
 
         $enseigants_fixture = new EnseignantFixtures();
         $enseigants_fixture->charger(
@@ -36,11 +42,17 @@ class AppFixtures extends Fixture
             $statuts_enseignant_fixture->list_statuts_enseignant
         );
 
+        var_dump('enseignant fini');
+
         $specialite_fixtures = new SpecialiteFixtures();
         $specialite_fixtures->charger($manager);
 
+        var_dump('specialités fini');
+
         $cursus_fixture = new CursusFixtures();
         $cursus_fixture->charger($manager);
+
+        var_dump('cursus fini');
 
         $formation_fixture = new FormationFixtures();
         for($i = 0; $i < count($cursus_fixture->list_cursus); $i++)
@@ -48,26 +60,28 @@ class AppFixtures extends Fixture
             $formation_fixture->charger($manager, $cursus_fixture->list_cursus[$i]);
         }
 
-        $etudiant_fixtures = new EtudiantFixtures();
-        $etudiant_fixtures->charger($manager, $personnes_fixture->list_personnes, 100, $formation_fixture->list_formations);
+        var_dump('formations fini');
 
-        var_dump('Etudiants fini!');
+        $etudiant_fixtures = new EtudiantFixtures();
+        $etudiant_fixtures->charger($manager, $personnes_fixture->list_personnes, 1000, $formation_fixture->list_formations);
+
+        var_dump('etudiants fini');
 
         $ues = new UEFixtures();
         $ues->charger($manager, $specialite_fixtures->list_specialites, $formation_fixture->list_formations);
 
-        var_dump('UEs fini!');
+        var_dump('ues fini');
 
         $uesValideFixtures = new UEValideFixtures();
         $uesValideFixtures->charger($manager, $etudiant_fixtures->list_etudiants, $ues->list_ues);
 
-        var_dump('UeValides fini!');
+        var_dump('ues valides fini');
 
         $cours_fixtures = new CourFixtures();
         $cours_fixtures->charger($manager, $enseigants_fixture->list_enseignants, $salleFixture->list_salles,
                                 $ues->list_ues);
 
-        var_dump('Cours fini!');
+        var_dump('cours fini');
 
         $manager->flush();
     }
