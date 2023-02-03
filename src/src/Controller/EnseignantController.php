cba71b4faf4b07ef90990e05d19508eea1f41257
MathieuDlp <contact.mathieudelepine@gmail.com>
2023-02-03 09:54:01 +0100

    WIP add edit on enseignant

--- src/src/Controller/EnseignantController.php
+++ src/src/Controller/EnseignantController.php
@@ -7,6 +7,7 @@ use App\Entity\Formation;
 use App\Entity\Personne;
 use App\Entity\Specialite;
 use App\Entity\StatutEnseignant;
+use App\Form\enseignant\EnseignantEditType;
 use App\Form\enseignant\EnseignantFilterType;
 use App\Form\enseignant\EnseignantType;
 use App\Form\enseignant\RechercheProfType;
@@ -97,7 +98,7 @@ class EnseignantController extends AbstractController
             if($formationres != 'non')
             {
                 // Prof responsable
-                dump("prof responsable");
+                $newEnseignant->setResponsableFormation($formationres);
             }
             $errors = $validator->validate($newPersonne);
             if(count($errors) <= 0){
@@ -107,7 +108,6 @@ class EnseignantController extends AbstractController
                 $this->addFlash('crud', "L'enseignant : {$form->get('Nom')->getData()} {$form->get('Prenom')->getData()}, a été créé avec succès.");
                 return $this->redirectToRoute('enseignant_list');
             } else {
-                $errorsString = (string) $errors;
                 return $this->render('enseignant/add.html.twig', [
                     'profForm' => $form->createView(),
                     'errors' => $errors,
@@ -118,12 +118,79 @@ class EnseignantController extends AbstractController
 
 
         return $this->render('enseignant/add.html.twig', [
-            'profForm' => $form->createView(),
+            'addEnseignantForm' => $form->createView(),
             'errors' => [],
         ]);
     }
 
+    #[Route('/edit/{id}', name: 'edit')]
+    public function edit(EntityManagerInterface $entityManager, Request $request, int $id, ValidatorInterface $validator): Response
+    {
+        $enseignant = $entityManager->getRepository(Enseignant::class)->find($id);
 
+        // Get all the Entity in the repos.
+        $listeStatus = $entityManager->getRepository(StatutEnseignant::class)->getAllForm();
+        $listeSpecialites = $entityManager->getRepository(Specialite::class)->getAllForm();
+        $listeFormations = $entityManager->getRepository(Formation::class)->getAllForm();
 
+        $form = $this->createForm(EnseignantType::class, null,[
+            'status' => $listeStatus,
+            'specialites' => $listeSpecialites,
+            'formations' => $listeFormations
+        ]);
 
+        // feed the form
+        $this->feedEditForm($form, $enseignant);
+
+        $form->add('send', SubmitType::class, ['label' => "Modifier l'enseignant"]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $personne = $enseignant->getPersonne();
+
+            // Update the personne and enseignant
+            $personne->setNom($form->get('Nom')->getData());
+            $personne->setPrenom($form->get('Prenom')->getData());
+            $enseignant->setStatutEnseignant($entityManager->getRepository(StatutEnseignant::class)->findOneBy(['id' => $form->get('Status')->getData()]));
+            $enseignant->setSection($entityManager->getRepository(Specialite::class)->findOneBy(['id' => $form->get('SectionDenseignement')->getData()]));
+            $formationres = $form->get('FormationRes')->getData();
+
+            if($formationres != 'non')
+            {
+                // Prof responsable
+                $enseignant->setResponsableFormation($formationres);
+            }
+
+            // Validation
+            $errors = $validator->validate($personne);
+            if(count($errors) <= 0) {
+                // No error :
+                $entityManager->persist($enseignant);
+                $entityManager->flush();
+
+                $this->addFlash('crud', "L'enseignant : {$form->get('Nom')->getData()} {$form->get('Prenom')->getData()}, a été modifié avec succès.");
+                return $this->redirectToRoute('enseignant_list');
+            } else {
+                // Error:
+                return $this->render('enseignant/edit.html.twig', [
+                    'editEnseignantForm' => $form->createView(),
+                    'errors' => $errors,
+                ]);
+            }
+
+        }
+
+        return $this->render('enseignant/edit.html.twig', [
+            'editEnseignantForm' => $form->createView(),
+            'errors' => [],
+        ]);
+    }
+
+    private function feedEditForm($form, $enseignant){
+        $form->get('Nom')->setData($enseignant->getPersonne()->getNom());
+        $form->get('Prenom')->setData($enseignant->getPersonne()->getPrenom());
+        $form->get('Status')->setData($enseignant->getStatutEnseignant()->getNom());
+        $form->get('SectionDenseignement')->setData($enseignant->getSection()->getNom());
+        $form->get('FormationRes')->setData($enseignant->getResponsableFormation());
+    }
 }
