8588a4ed11eddc78a92b184efc66a6c9bcac9460
MathieuDlp <contact.mathieudelepine@gmail.com>
2023-02-09 14:51:11 +0100

    fiw trigger on personne mail

--- src/sql_script/triggers/pre_persist_personne_mail.sql
+++ src/sql_script/triggers/pre_persist_personne_mail.sql
@@ -5,10 +5,12 @@ CREATE TRIGGER pre_persist_personne_mail BEFORE INSERT ON personne
     FOR EACH ROW
 BEGIN
     DECLARE nbPersonneSameMail INT DEFAULT 0;
+    DECLARE myRegex VARCHAR(255);
+
+    SET myRegex = (CONCAT('^',NEW.prenom,'\.',NEW.nom, '[a-zA-Z]+[0-9]*@univ-poitiers\.fr$' ));
     -- Compte les personnes ayant le meme mail que la nouvelle personne
     SELECT COUNT(*) INTO nbPersonneSameMail FROM personne
-        WHERE personne.email REGEXP '^[a-zA-Z]+\.[a-zA-Z]+[0-9]*@univ-poitiers\.fr$';
-
+        WHERE personne.email REGEXP myRegex;
     IF nbPersonneSameMail > 0 THEN -- change mail
         SET NEW.email = (CONCAT(NEW.prenom,'.',NEW.nom, nbPersonneSameMail, '@univ-poitiers.fr' ));
     END IF;
