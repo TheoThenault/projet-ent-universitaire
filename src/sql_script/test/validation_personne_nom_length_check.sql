9e23009bc103969525a60cf21bd603ad7f471a91
TheoThenault <theo.thenault@gmail.com>
2023-02-10 15:15:49 +0100

    validetaion_check_personne_nom

---
+++ src/sql_script/test/validation_personne_nom_length_check.sql
@@ -0,0 +1,34 @@
+DROP PROCEDURE IF EXISTS validation_personne_nom_length_check;
+
+DELIMITER //
+CREATE PROCEDURE validation_personne_nom_length_check(_nom VARCHAR(260))
+BEGIN
+    DECLARE errorMessage VARCHAR(255);
+    -- Si une exception est levée, capturer le message d'erreur
+    BEGIN
+        DECLARE EXIT HANDLER FOR SQLEXCEPTION
+            BEGIN
+                SET errorMessage := 'Test personne_nom_length_check';
+            END;         
+           
+		INSERT INTO projet_ent_universitaire.personne(etudiant_id, enseignant_id, email, nom, prenom, password, roles)
+			VALUES(NULL, NULL, 'email@internet.com', _nom, 'prenom', 
+			'$2y$13$PQfkvYMxBXDalJ5hP9kilue8jeJarc3wGnCwvtzxg7noPPYOIZCv6', 
+						'a:1:{i:0;s:9:"ROLE_USER";}');
+
+           
+    END;
+
+    -- Vérifier si un message d'erreur a été capturé avec deux cours ayant le meme enseignant_id et creneau
+    IF errorMessage IS NOT NULL THEN
+        INSERT INTO test (nom, resultat)
+        VALUES (errorMessage, 'OK');
+    ELSE
+        INSERT INTO test (nom, resultat)
+        VALUES ('Test personne_nom_length_check', 'FAIL');
+    END IF;
+END; //
+DELIMITER ;
+
+CALL validation_personne_nom_length_check('t');
+CALL validation_personne_nom_length_check('tttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt');
