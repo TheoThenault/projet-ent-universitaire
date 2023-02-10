d0b5fd8769e91161ec00311d6ccd06ea92498ae8
MathieuDlp <contact.mathieudelepine@gmail.com>
2023-02-10 14:49:07 +0100

    added test

--- src/sql_script/peuplement/salle.sql
+++ src/sql_script/peuplement/salle.sql
@@ -16,47 +16,46 @@ BEGIN
 
     CREATE TEMPORARY TABLE nomBatTemp(nom VARCHAR(255));
         -- insert rows to the table nomBat
-INSERT INTO nomBatTemp(nom)
-VALUES ("H01"),
-       ("B02"),
-       ("G03"),
-       ("A04"),
-       ("J05");
+    INSERT INTO nomBatTemp(nom)
+    VALUES ("H01"),
+           ("B02"),
+           ("G03"),
+           ("A04"),
+           ("J05");
 
-CREATE TEMPORARY TABLE equipementTemp(nom VARCHAR(255));
-        -- insert rows to the table equipement
-INSERT INTO equipementTemp(nom)
-VALUES ("Informatique"),
-       ("Langue"),
-       ("Chimie"),
-       ("Physique");
+    CREATE TEMPORARY TABLE equipementTemp(nom VARCHAR(255));
+            -- insert rows to the table equipement
+    INSERT INTO equipementTemp(nom)
+    VALUES ("Informatique"),
+           ("Langue"),
+           ("Chimie"),
+           ("Physique");
 
+    CREATE TEMPORARY TABLE capaciteTemp(cap INT);
+    INSERT INTO capaciteTemp(cap)
+    VALUES (20),
+           (30),
+           (40),
+           (50),
+           (100);
 
-CREATE TEMPORARY TABLE capaciteTemp(cap INT);
-INSERT INTO capaciteTemp(cap)
-VALUES (20),
-       (30),
-       (40),
-       (50),
-       (100);
+    WHILE i < nbSalle DO
+        SELECT * INTO nomBat FROM nomBatTemp ORDER BY RAND() LIMIT 1;
 
-WHILE i < nbSalle DO
+        SET nom = CONCAT(nomBat, " ", i);
 
-SELECT * INTO nomBat FROM nomBatTemp ORDER BY RAND() LIMIT 1;
+        SELECT * INTO equipement FROM equipementTemp ORDER BY RAND() LIMIT 1;
+        SELECT * INTO capacite FROM capaciteTemp ORDER BY RAND() LIMIT 1;
 
-SET nom = CONCAT(nomBat, " ", i);
+        INSERT INTO salle(nom, batiment, equipement, capacite)
+        VALUES(nom, nomBat, equipement, capacite );
 
-SELECT * INTO equipement FROM equipementTemp ORDER BY RAND() LIMIT 1;
+        SET i = i + 1;
 
-SELECT * INTO capacite FROM capaciteTemp ORDER BY RAND() LIMIT 1;
+    END WHILE;
 
-INSERT INTO salle(nom, batiment, equipement, capacite)
-VALUES(nom, nomBat, equipement, capacite );
-
-SET i = i + 1;
-
-END WHILE;
-
+    INSERT INTO salle(id, nom, batiment, equipement, capacite)
+    VALUES(99999999,'salleForTests', 'batimentTest', null, 0 );
 END; //
 
 
---
+++ src/sql_script/test/test_VerifSalleQuandCreationCour.sql
@@ -0,0 +1,30 @@
+DROP PROCEDURE IF EXISTS test_VerifSalleQuandCreationCour;
+
+DELIMITER //
+CREATE PROCEDURE test_VerifSalleQuandCreationCour()
+BEGIN
+    DECLARE errorMessage VARCHAR(255);
+    -- Si une exception est levée, capturer le message d'erreur
+    BEGIN
+        DECLARE EXIT HANDLER FOR SQLEXCEPTION
+            BEGIN
+                SET errorMessage := 'Test du déclencheur VerifSalleQuandCreationCour';
+            END;
+
+        -- Tenter d'insérer un cours avec une salle pouvant acceuillir pas plus de 0 personne
+        INSERT INTO cour (ue_id, salle_id, enseignant_id, groupe_id, creneau)
+        VALUES (1, 99999999, 1, 1, 599);
+    END;
+
+    -- Vérifier si un message d'erreur a été capturé un cours dans une salle déjà pleine
+    IF errorMessage IS NOT NULL THEN
+        INSERT INTO test (nom, resultat)
+        VALUES (errorMessage, 'OK');
+    ELSE
+        INSERT INTO test (nom, resultat)
+        VALUES ('Test du déclencheur VerifSalleQuandCreationCour', 'FAIL');
+    END IF;
+END; //
+DELIMITER ;
+
+CALL test_VerifSalleQuandCreationCour();
