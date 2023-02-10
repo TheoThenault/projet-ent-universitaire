0fdd895d9316fb20011157c203ba726c7d5a4ae0
TheoThenault <theo.thenault@gmail.com>
2023-02-10 15:54:33 +0100

    validation_formation_nom_length_check

---
+++ src/sql_script/test/validation_formation_nom_length_check.sql
@@ -0,0 +1,30 @@
+DROP PROCEDURE IF EXISTS validation_formation_nom_length_check;
+
+DELIMITER //
+CREATE PROCEDURE validation_formation_nom_length_check(_nom VARCHAR(260))
+BEGIN
+    DECLARE errorMessage VARCHAR(255);
+    -- Si une exception est levée, capturer le message d'erreur
+    BEGIN
+        DECLARE EXIT HANDLER FOR SQLEXCEPTION
+            BEGIN
+                SET errorMessage := 'Test formation_nom_length_check';
+            END;         
+		INSERT INTO projet_ent_universitaire.formation(cursus_id, nom, annee)
+			VALUES(1, _nom, 1);
+
+    END;
+
+    -- Vérifier si un message d'erreur a été capturé avec deux cours ayant le meme enseignant_id et creneau
+    IF errorMessage IS NOT NULL THEN
+        INSERT INTO test (nom, resultat)
+        VALUES (errorMessage, 'OK');
+    ELSE
+        INSERT INTO test (nom, resultat)
+        VALUES ('Test chk_groupe_type', 'FAIL');
+    END IF;
+END; //
+DELIMITER ;
+
+CALL validation_formation_nom_length_check('t');
+CALL validation_formation_nom_length_check('tttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt');
