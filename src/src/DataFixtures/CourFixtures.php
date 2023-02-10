d5a9155d86622af2fff881b54b8e35f0417ee654
TheoThenault <theo.thenault@gmail.com>
2023-02-10 17:09:55 +0100

    cour pas deux eleves

--- src/src/DataFixtures/CourFixtures.php
+++ src/src/DataFixtures/CourFixtures.php
@@ -17,6 +17,7 @@ class CourFixtures
     public function charger(ObjectManager $manager, array $list_enseignant, array $list_salles, array $list_ues): void
     {
         $listHeuresProf = array();
+        $groupes_creneaux = [];
 
         $manager->flush();
         $list_groupes = $manager->getRepository(Groupe::class)->findAll();
@@ -48,7 +49,8 @@ class CourFixtures
             $_ue = rand(0, $nU-1);
 
             $cour = new Cour();
-            $cour->setCreneau(($i % 600) + 1);
+            $current_creneau = ($i % 600) + 1;
+            $cour->setCreneau($current_creneau);
             $cour->setEnseignant($prof);
             $cour->setSalle($list_salles[$sall]);
             $cour->setUe($list_ues[$_ue]);
@@ -68,7 +70,18 @@ class CourFixtures
                 $i--;
                 continue;
             }
-            $cour->setGroupe($grps[array_rand($grps)]);
+
+            $chosenGRP = $grps[array_rand($grps)];
+            $grpID = $chosenGRP->getId();
+            if(array_key_exists($grpID.'-'.$current_creneau, $groupes_creneaux))
+            {
+                $i--;
+                continue;
+            }else{
+                $groupes_creneaux[$grpID.'-'.$current_creneau] = 0;
+            }
+
+            $cour->setGroupe($chosenGRP);
             //$cour->setUe($list_ues[$i % $nU]);
             $this->list_cours[] = $cour;
             $manager->persist($cour);
