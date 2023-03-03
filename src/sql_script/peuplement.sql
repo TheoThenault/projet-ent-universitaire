28459dd02bb9784dfcc19964553cf015dcf14d4c
MathieuDlp <contact.mathieudelepine@gmail.com>
2023-03-03 17:33:00 +0100

    FIX enseignant.sql

--- src/sql_script/peuplement.sql
+++ src/sql_script/peuplement.sql
@@ -304,86 +304,61 @@ DROP PROCEDURE IF EXISTS peupleEnseignant //
 
 CREATE PROCEDURE peupleEnseignant()
 BEGIN
-
-    DECLARE counter INT DEFAULT 2; -- La premiere formation est pour un prof spécifique
+    DECLARE counter INT DEFAULT 1; -- La premiere formation est pour un prof spécifique
     DECLARE nbFormation INT;
     DECLARE lengthListePersonne INT;
     DECLARE statutEnseignantId INT;
     DECLARE specialiteId INT;
 
--- Création d'un enseignants responsable spécifique
-SELECT id INTO statutEnseignantId FROM statut_enseignant ORDER BY RAND() LIMIT 1;
-SELECT id INTO specialiteId FROM specialite ORDER BY RAND() LIMIT 1;
+    -- Création des enseignants responsable
+    SELECT COUNT(*) INTO nbFormation FROM formation;
+    WHILE counter < nbFormation + 1 DO
 
-INSERT INTO enseignant(statut_enseignant_id, section_id, responsable_formation_id)
-VALUES (
-           statutEnseignantId,
-           specialiteId,
-           1
-       );
-UPDATE personne SET enseignant_id = LAST_INSERT_ID() WHERE email = 'enseignant.res@univ-poitiers.fr';
+        SELECT id INTO statutEnseignantId FROM statut_enseignant ORDER BY RAND() LIMIT 1;
+        SELECT id INTO specialiteId FROM specialite ORDER BY RAND() LIMIT 1;
 
+        INSERT INTO enseignant(statut_enseignant_id, section_id, responsable_formation_id)
+            VALUES (
+                statutEnseignantId,
+                specialiteId,
+                counter
+            );
 
--- Création des enseignants responsable
+        UPDATE personne
+        SET enseignant_id = LAST_INSERT_ID(),
+            roles = 'a:1:{i:0;s:9:"ROLE_ENSEIGNANT_RES";}'
+        WHERE id = counter;
 
-SELECT COUNT(*) INTO nbFormation FROM formation;
-WHILE counter < nbFormation + 1 DO
+        SET counter = counter + 1;
+    END WHILE;
 
-SELECT id INTO statutEnseignantId FROM statut_enseignant ORDER BY RAND() LIMIT 1;
-SELECT id INTO specialiteId FROM specialite ORDER BY RAND() LIMIT 1;
+    -- Création des enseignants
 
-INSERT INTO enseignant(statut_enseignant_id, section_id, responsable_formation_id)
-VALUES (
-           statutEnseignantId,
-           specialiteId,
-           counter
-       );
+    SELECT COUNT(*) FROM personne INTO lengthListePersonne;
 
-UPDATE personne SET enseignant_id = LAST_INSERT_ID(), roles = 'a:1:{i:0;s:9:"ROLE_ENSEIGNANT_RES";}' WHERE id = counter;
+    WHILE counter < (lengthListePersonne / 4) DO -- Mettre 1/4 de prof pour 3/4 étudiants
 
-SET counter = counter + 1;
+        SELECT id INTO statutEnseignantId FROM statut_enseignant ORDER BY RAND() LIMIT 1;
+        SELECT id INTO specialiteId FROM specialite ORDER BY RAND() LIMIT 1;
 
-END WHILE;
+        INSERT INTO enseignant(statut_enseignant_id, section_id)
+            VALUES (
+                statutEnseignantId,
+                specialiteId
+            );
 
--- Création des enseignants
+        UPDATE personne
+        SET enseignant_id = LAST_INSERT_ID(),
+            roles = 'a:1:{i:0;s:9:"ROLE_ENSEIGNANT";}'
+        WHERE id = counter;
 
-SELECT COUNT(*) FROM personne INTO lengthListePersonne;
-
-WHILE counter < (lengthListePersonne / 4) DO -- Mettre 1/4 de prof pour 3/4 étudiants
-
-SELECT id INTO statutEnseignantId FROM statut_enseignant ORDER BY RAND() LIMIT 1;
-SELECT id INTO specialiteId FROM specialite ORDER BY RAND() LIMIT 1;
-
-INSERT INTO enseignant(statut_enseignant_id, section_id)
-VALUES (
-           statutEnseignantId,
-           specialiteId
-       );
-
-UPDATE personne SET enseignant_id = LAST_INSERT_ID(), roles = 'a:1:{i:0;s:9:"ROLE_ENSEIGNANT";}' WHERE id = counter;
-
-SET counter = counter + 1;
-
-END WHILE;
-
--- Création d'un enseignant spécifique
-
-SELECT id INTO statutEnseignantId FROM statut_enseignant ORDER BY RAND() LIMIT 1;
-SELECT id INTO specialiteId FROM specialite ORDER BY RAND() LIMIT 1;
-
-INSERT INTO enseignant(statut_enseignant_id, section_id)
-VALUES (
-           statutEnseignantId,
-           specialiteId
-       );
-UPDATE personne SET enseignant_id = LAST_INSERT_ID() WHERE email = 'enseignant@univ-poitiers.fr';
-
+        SET counter = counter + 1;
+    END WHILE;
 END; //
 
 DELIMITER ;
 
 CALL peupleEnseignant();
-
 
 -- =============================
 -- 8) peupleEtudiant
