b926e2a01d867403a018ee67bc4b5644bd02d274
Simon Merlaud <simon.merlaud@gmail.com>
2023-03-05 15:18:33 +0100

    Fix Ne peut plus valider 2 fois la même ue à un étudiant

--- src/sql_script/peuplement/ueValide.sql
+++ src/sql_script/peuplement/ueValide.sql
@@ -10,31 +10,31 @@ BEGIN
 
     WHILE counter < 20 DO -- 20 est le nombre d'ue valide
 
-        -- prend 1 random dans la liste des étudiants
-        SELECT id INTO etuId FROM etudiant ORDER BY RAND() LIMIT 1;
+    -- prend 1 random dans la liste des étudiants
+            SELECT id INTO etuId FROM etudiant ORDER BY RAND() LIMIT 1;
 
-        CREATE TABLE ueTmp(id INT) AS
-        SELECT u.id
-        FROM ue u
-                 LEFT JOIN formation f
-                           ON f.id = u.formation_id
-                 LEFT JOIN etudiant e
-                           ON e.formation_id = f.id
-        WHERE e.id = etuId;
+            CREATE TEMPORARY TABLE ueTmp(id INT) AS
+            SELECT u.id
+            FROM ue u
+                     LEFT JOIN formation f
+                               ON f.id = u.formation_id
+                     LEFT JOIN etudiant e
+                               ON e.formation_id = f.id
+            WHERE e.id = etuId;
 
-        -- réccup les ue de l'étudiants
-        SELECT id INTO ueId FROM ueTmp ORDER BY RAND() LIMIT 1;
+            -- réccup les ue de l'étudiants
+            SELECT id INTO ueId FROM ueTmp ORDER BY RAND() LIMIT 1;
 
 
-        -- prend au hasard 1 eu
-        INSERT INTO etudiant_ue(etudiant_id, ue_id)
-            VALUES(etuId, ueId);
-        -- valide cette eu( donc mettre id etudiant et id de l'ue)
+            -- prend au hasard 1 eu
+            INSERT IGNORE INTO etudiant_ue(etudiant_id, ue_id) -- Le IGNORE permet d'ignorer un insertion si elle exsite deja
+                VALUES(etuId, ueId);
+                -- valide cette eu( donc mettre id etudiant et id de l'ue)
 
-        DROP TABLE ueTMP;
+            DROP TABLE ueTMP;
 
-        SET counter = counter + 1;
-    END WHILE;
+            SET counter = counter + 1;
+        END WHILE;
 
 END; //
 
